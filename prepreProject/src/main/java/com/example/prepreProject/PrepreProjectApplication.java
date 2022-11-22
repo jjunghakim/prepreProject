8246faa64ea013734b52c6844b0433a4c4dcd9bb
jjunghakim <wlikeoxy@gmail.com>
2022-11-22 16:04:07 +0900

    Initial commit

---
+++ prepreProject/src/main/java/com/example/prepreProject/PrepreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.prepreProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepreProjectApplication.class, args);
+	}
+
+}
