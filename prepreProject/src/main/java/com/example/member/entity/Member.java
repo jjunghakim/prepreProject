73f650d810a744e6c49340d8b819991b4b8c19f6
jjunghakim <wlikeoxy@gmail.com>
2022-12-06 23:08:43 +0900

    12/06 Entity 클래스들(Member, Order, Coffee) 테이블 및 컬럼 매핑

--- prepreProject/src/main/java/com/example/member/entity/Member.java
+++ prepreProject/src/main/java/com/example/member/entity/Member.java
@@ -5,13 +5,37 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "USERS")
+@Table(name = "USERS")
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long memberId;
+
+    @Column(nullable = false, updatable = false, unique = true)
     private String email;
+
+    @Column(length = 100, nullable = false)
     private String name;
+
+    @Column(length = 13, nullable = false, unique = true)
     private String phone;
+
+    @Column(nullable = false)
+    private LocalDateTime createAt = LocalDateTime.now();
+
+    @Column(nullable = false, name = "LAST_MODIFIED_AT")
+    private LocalDateTime modifiedAt = LocalDateTime.now();
+
+    @Transient
+    private String age;
+
 }
+
--- prepreProject/src/main/java/com/example/order/entity/Order.java
+++ prepreProject/src/main/java/com/example/order/entity/Order.java
@@ -4,15 +4,45 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import net.bytebuddy.asm.Advice;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "ORDERS")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long orderId;
-    private LocalDateTime createdAt;
 
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus = OrderStatus.ORDER_REQUEST;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = false, name = "LAST_MODIFIED_AT")
+    private LocalDateTime modifiedAt = LocalDateTime.now();
+
+    public enum OrderStatus{
+        ORDER_REQUEST(1, "주문 요청"),
+        ORDER_CONFIRM(2, "주문 요청"),
+        ORDER_COMPLETE(3, "주문 요청"),
+        ORDER_CANCEL(4, "주문 요청");
+
+        @Getter
+        private int stepNumber;
+
+        @Getter
+        private String stepDescription;
+
+        OrderStatus(int stepNumber, String stepDescription){
+            this.stepNumber = stepNumber;
+            this.stepDescription = stepDescription;
+        }
+    }
 }
